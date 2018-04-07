1ec259300496532b0ebccfa7087133f860022832
eletitos <eletitos@gmail.com>
2018-04-08 00:48:26 +0200

    Incorporación página secundaria fotos

--- js/datos.js
+++ js/datos.js
@@ -4,490 +4,560 @@ var jsonDatos = [
        "numero": 1,
        "titulo": "LA CIUDAD CÚBICA",
        "archivo": "1.gif",
-       "clases": "animation illustration digital geometry"
+       "clases": "animation illustration digital geometry",
+       "menu": "motion"
     },
     {
        "nombre": "img2",
        "numero": 2,
        "titulo": "PISOTEO",
        "archivo": "2.jpg",
-       "clases": "animation illustration digital"
+       "clases": "animation illustration digital",
+       "menu": "illustration"
     },
     {
        "nombre": "img3",
        "numero": 3,
        "titulo": "ALMUDENA",
        "archivo": "3.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img4",
        "numero": 4,
        "titulo": "ESCENARIO PRIMAVERA",
        "archivo": "4.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img5",
        "numero": 5,
        "titulo": "VERANO",
        "archivo": "5.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "motion"
     },
     {
        "nombre": "img6",
        "numero": 6,
        "titulo": "OLEADA",
        "archivo": "6.gif",
-       "clases": "animation illlustration digital"
+       "clases": "animation illlustration digital",
+       "menu": "motion"
     },
     {
        "nombre": "img7",
        "numero": 7,
        "titulo": "ANTOÑICO",
        "archivo": "7.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img8",
        "numero": 8,
        "titulo": "ARDE ARCADE",
        "archivo": "8.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img9",
        "numero": 9,
        "titulo": "CON VISTAS AL ARTE",
        "archivo": "9.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img10",
        "numero": 10,
        "titulo": "LA CABRA, LA CABRA",
        "archivo": "10.jpg",
-       "clases": "illustration digital geometry"
+       "clases": "illustration digital geometry",
+       "menu": "illustration"
     },
     {
        "nombre": "img11",
        "numero": 11,
        "titulo": "CAMELAR",
        "archivo": "11.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img12",
        "numero": 12,
        "titulo": "NOVANA",
        "archivo": "12.jpg",
-       "clases": "photo digital poster"
+       "clases": "photo digital poster",
+       "menu": "poster"
     },
     {
        "nombre": "img13",
        "numero": 13,
        "titulo": "LA FUTURA DE TODA LA VIDA",
        "archivo": "13.jpg",
-       "clases": "digital poster"
+       "clases": "digital poster",
+       "menu": "poster"
     },
     {
        "nombre": "img14",
        "numero": 14,
        "titulo": "CAZA MAYOR",
        "archivo": "14.jpg",
-       "clases": "digital illustration "
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img15",
        "numero": 15,
        "titulo": "SE BUSCAN",
        "archivo": "15.gif",
-       "clases": "animation photo"
+       "clases": "animation photo",
+       "menu": "motion"
     },
     {
        "nombre": "img16",
        "numero": 16,
        "titulo": "MIL NOVECIENTOS OCHENTAYCINCO",
        "archivo": "16.jpg",
-       "clases": "poster digital"
+       "clases": "poster digital",
+       "menu": "poster"
     },
     {
        "nombre": "img17",
        "numero": 17,
        "titulo": "PARTIDA EN DOS",
        "archivo": "17.jpg",
-       "clases": "photo collage"
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img18",
        "numero": 18,
        "titulo": "HOLA CARACOLA",
        "archivo": "18.gif",
-       "clases": "generative geometry animation digital"
+       "clases": "generative geometry animation digital",
+       "menu": "motion"
     },
     {
        "nombre": "img19",
        "numero": 19,
        "titulo": "CRYSTAL FIGHTERS",
        "archivo": "19.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img20",
        "numero": 20,
        "titulo": "CUANDO CALLAS",
        "archivo": "20.jpg",
-       "clases": "illustration digital  "
+       "clases": "illustration digital",
+       "menu": "illustration"
     },
     {
        "nombre": "img21",
        "numero": 21,
        "titulo": "BROKEN MIRROR",
        "archivo": "21.jpg",
-       "clases": "photo collage geometry"
+       "clases": "photo collage geometry",
+       "menu": "photo"
     },
     {
        "nombre": "img22",
        "numero": 22,
        "titulo": "¿FRIGODEDO O FRIGOPIÉ?",
        "archivo": "22.gif",
-       "clases": "animation digital illustration"
+       "clases": "animation digital illustration",
+       "menu": "motion"
     },
     {
        "nombre": "img23",
        "numero": 23,
        "titulo": "MUJERES",
        "archivo": "23.jpg",
-       "clases": "drawing illustration"
+       "clases": "drawing illustration",
+       "menu": "drawing"
     },
     {
        "nombre": "img24",
        "numero": 24,
        "titulo": "ABRÁZAME",
        "archivo": "24.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img25",
        "numero": 25,
        "titulo": "GUERNICA EN MOVIMIENTO I",
        "archivo": "25.gif",
-       "clases": "animation digital  "
+       "clases": "animation digital",
+       "menu": "motion"
     },
     {
        "nombre": "img26",
        "numero": 26,
        "titulo": "EL PRINCIPITO",
        "archivo": "26.jpg",
-       "clases": "digital illustration "
+       "clases": "digital illustration",
+       "menu": "motion"
     },
     {
        "nombre": "img27",
        "numero": 27,
        "titulo": "DE COLORES",
        "archivo": "27.jpg",
-       "clases": "photo collage"
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img28",
        "numero": 28,
        "titulo": "ENRIC EL SABIO",
        "archivo": "28.jpg",
-       "clases": "photo"
+       "clases": "photo",
+       "menu": "photo"
     },
     {
        "nombre": "img29",
        "numero": 29,
        "titulo": "SILENCIO TRIANGULAR",
        "archivo": "29.jpg",
-       "clases": "photo collage geometry"
+       "clases": "photo collage geometry",
+       "menu": "photo"
     },
     {
        "nombre": "img30",
        "numero": 30,
        "titulo": "A CUADROS",
        "archivo": "30.gif",
-       "clases": "pattern animation digital geometry"
+       "clases": "pattern animation digital geometry",
+       "menu": "motion"
     },
     {
        "nombre": "img31",
        "numero": 31,
        "titulo": "DISTORSIÓN ",
-       "archivo": "30.jpg",
-       "clases": "photo collage"
+       "archivo": "31.jpg",
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img32",
        "numero": 32,
        "titulo": "LA TÍA MISERIA",
        "archivo": "32.jpg",
-       "clases": "illustration digital"
+       "clases": "illustration digital",
+       "menu": "illustration"
     },
     {
        "nombre": "img33",
        "numero": 33,
        "titulo": "ADIOS A LA BOHEME",
        "archivo": "33.jpg",
-       "clases": "drawing illustration"
+       "clases": "drawing illustration",
+       "menu": "drawing"
     },
     {
        "nombre": "img34",
        "numero": 34,
        "titulo": "NOVANA Y LOS SECRETOS",
        "archivo": "34.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img35",
        "numero": 35,
        "titulo": "SERGIO EL SABIO",
        "archivo": "35.jpg",
-       "clases": "photo"
+       "clases": "photo",
+       "menu": "photo"
     },
     {
        "nombre": "img36",
        "numero": 36,
        "titulo": "HOMENAJE A FORGES",
        "archivo": "36.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img37",
        "numero": 37,
        "titulo": "SOY PERRO",
        "archivo": "37.jpg",
-       "clases": "illustration digital  "
+       "clases": "illustration digital",
+       "menu": "illustration"
     },
     {
        "nombre": "img38",
        "numero": 38,
        "titulo": "GUAU!",
-       "archivo": "38.jpg",
-       "clases": "animation geometry digital "
+       "archivo": "38.gif",
+       "clases": "animation geometry digital",
+       "menu": "motion"
     },
     {
        "nombre": "img39",
        "numero": 39,
        "titulo": "LAURA",
        "archivo": "39.jpg",
-       "clases": "photo"
+       "clases": "photo",
+       "menu": "photo"
     },
     {
        "nombre": "img40",
        "numero": 40,
        "titulo": "PILAR",
        "archivo": "40.jpg",
-       "clases": "photo collage"
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img41",
        "numero": 41,
        "titulo": "EN LA ESPIRAL",
        "archivo": "41.gif",
-       "clases": "animation generative digital geometry"
+       "clases": "animation generative digital geometry",
+       "menu": "motion"
     },
     {
        "nombre": "img42",
        "numero": 42,
        "titulo": "GUERNICA EN MOVIMIENTO II",
        "archivo": "42.gif",
-       "clases": "animation "
+       "clases": "animation",
+       "menu": "motion"
     },
     {
        "nombre": "img43",
        "numero": 43,
        "titulo": "BLINDLESS",
        "archivo": "42.jpg",
-       "clases": "photo   "
+       "clases": "photo",
+       "menu": "motion"
     },
     {
        "nombre": "img44",
        "numero": 44,
        "titulo": "YO, PAYASO",
        "archivo": "45.jpg",
-       "clases": "drawing illustration"
+       "clases": "drawing illustration",
+       "menu": "drawing"
     },
     {
        "nombre": "img45",
        "numero": 45,
        "titulo": "MUJER EN ROSA",
        "archivo": "46.jpg",
-       "clases": "illustration digital geometry"
+       "clases": "illustration digital geometry",
+       "menu": "illustration"
     },
     {
        "nombre": "img46",
        "numero": 46,
        "titulo": "NEGRO OSCURO",
        "archivo": "46.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img47",
        "numero": 47,
        "titulo": "UN PERRO VERDE",
        "archivo": "47.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img48",
        "numero": 48,
        "titulo": "PELOMONO",
        "archivo": "48.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img49",
        "numero": 49,
        "titulo": "VECINOS",
        "archivo": "49.jpg",
-       "clases": "poster digital"
+       "clases": "poster digital",
+       "menu": "poster"
     },
     {
        "nombre": "img50",
        "numero": 50,
        "titulo": "RUTINAS",
        "archivo": "50.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img51",
        "numero": 51,
        "titulo": "MUJERES Y SECRETOS",
        "archivo": "51.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img52",
        "numero": 52,
        "titulo": "SOFAR SOUND",
        "archivo": "52.jpg",
-       "clases": "poster digital"
+       "clases": "poster digital",
+       "menu": "poster"
     },
     {
        "nombre": "img53",
        "numero": 53,
        "titulo": "LADO LUZ",
        "archivo": "53.jpg",
-       "clases": "photo"
+       "clases": "photo",
+       "menu": "photo"
     },
     {
        "nombre": "img54",
        "numero": 54,
        "titulo": "BOWIE",
        "archivo": "54.gif",
-       "clases": "animation digital ilustration"
+       "clases": "animation digital ilustration",
+       "menu": "motion"
     },
     {
        "nombre": "img55",
        "numero": 55,
        "titulo": "CALCOTADA",
        "archivo": "55.jpg",
-       "clases": "lettering digital"
+       "clases": "lettering digital",
+       "menu": "lettering"
     },
     {
        "nombre": "img56",
        "numero": 56,
        "titulo": "TOCAR, TOCAR, TOCAR",
        "archivo": "56.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img57",
        "numero": 57,
        "titulo": "BAÑISTA",
        "archivo": "57.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img58",
        "numero": 58,
        "titulo": "LOS OCHENTA",
        "archivo": "58.gif",
-       "clases": "animation digital geomerative geometry"
+       "clases": "animation digital geomerative geometry",
+       "menu": "motion"
     },
     {
        "nombre": "img59",
        "numero": 59,
        "titulo": "SE ALQUILA CORAZÓN",
        "archivo": "59.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img60",
        "numero": 60,
        "titulo": "CAT",
        "archivo": "60.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img61",
        "numero": 61,
        "titulo": "DISTORSIÓN II",
        "archivo": "61.jpg",
-       "clases": "photo collage"
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img62",
        "numero": 62,
        "titulo": "ME LLORAN LOS CUADROS",
        "archivo": "62.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img63",
        "numero": 63,
        "titulo": "DISTORSIÓN III",
        "archivo": "63.jpg",
-       "clases": "photo collage"
+       "clases": "photo collage",
+       "menu": "photo"
     },
     {
        "nombre": "img64",
        "numero": 64,
        "titulo": "EXTREMOS",
        "archivo": "64.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img65",
        "numero": 65,
        "titulo": "GLORIETA QUEVEDO",
        "archivo": "65.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img66",
        "numero": 66,
        "titulo": "LAPAVIÉS",
        "archivo": "66.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     },
     {
        "nombre": "img67",
        "numero": 67,
        "titulo": "BORU",
        "archivo": "67.gif",
-       "clases": "photo animation"
+       "clases": "photo animation",
+       "menu": "photo"
     },
     {
        "nombre": "img68",
        "numero": 68,
        "titulo": "TROFEOS",
        "archivo": "68.jpg",
-       "clases": "digital illustration"
+       "clases": "digital illustration",
+       "menu": "illustration"
     },
     {
        "nombre": "img69",
        "numero": 69,
        "titulo": "R2D2",
        "archivo": "69.gif",
-       "clases": "digital illustration animation geometry"
+       "clases": "digital illustration animation geometry",
+       "menu": "motion"
     },
     {
        "nombre": "img70",
        "numero": 70,
        "titulo": "EL OJO DE LAPHILLE",
        "archivo": "70.jpg",
-       "clases": "drawing usk"
+       "clases": "drawing usk",
+       "menu": "drawing"
     }
  ]
 
