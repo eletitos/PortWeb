c06d47c4d643438f3ba1d47745c8fcf9cde07941
eletitos <eletitos@gmail.com>
2018-04-10 17:51:48 +0200

    Cambio de jquery a vanilla

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-$(document).ready(function () {
+//
 
-    var contenedor = $('#contenedor');
+    var contenedor = document.getElementById('contenedor');
     var numeroFotos = 70;
     var extension;
     var numeroColumnas;
     var anchoContenedor;
     var anchoColumnas;
     var arrayMinimos = [];
-    var imagen = $('.imagen');
+    var imagen = document.querySelectorAll('.imagen');     //lista
     var cortinaBlanca;
     var primerScroll = true;
     var numRueda= 1;
-    var altoVentana = $(window).height();
+    var altoVentana = window.innerHeight;
     var botonMenu = document.getElementById('menu-icon');
     var scrollPosition = 0;
     var modoMosaico = true;
     var aleatorio;
     var dispositivoMovil = false;
     var menuDesplegado = false;
-    var about = $('#btn-about');
+    var about = document.getElementById('btn-about');
     var aboutDesplegado = false;
-    var altoMenu = $('header nav').height();
+    //var altoMenu = document.querySelector('header nav').offsetHeight;
+    var menu = document.querySelector('header nav');
+    var cortina = document.querySelectorAll('.cortina');
+    var cortinaRoja = document.querySelector('.cortina-roja');
+    var iconosRedes = document.querySelectorAll('.iconos-redes');
+    var textoAbout =  document.querySelector('.texto-about');
+    
    
     
 
@@ -28,135 +34,113 @@ $(document).ready(function () {
     calculoPosicion();
 
     for (let i = 0; i < datos.length; i++) {
-        $('.cortina').eq(i).html('<h3 class="titulo">'+datos[i].titulo +'</h3>');
+        cortina[i].innerHTML = '<h3 class="titulo">'+datos[i].titulo +'</h3>';
 
     }
     
 /*------------------------EVENTOS----------------------------------------*/  
-    $(window).resize(function () {  //Evento para cuando se redimensiona la pantalla
-    
-        for (let n = 0; n < numeroColumnas; n++) {        //Inicializando el array de los valores de columna.
-            arrayMinimos.pop(); 
-        }
+
+    window.addEventListener('resize', function(){
+            arrayMinimos =[];
             calcularColumnas();
             calculoPosicion();
-        });
+    });
 
-       
-        
-    $(window).scroll(function () {  
-        
+    window.addEventListener('scroll', function () { 
         if(botonMenu.checked || !modoMosaico){                          //Se fija el scroll si el menú está desplegado.
-            $(window).scrollTop(scrollPosition);
+            //window.scrollY=scrollPosition;
+            window.scrollTo(0, scrollPosition);
         }else{
-            for (let n = 0; n < numeroColumnas; n++) {        //Inicializando el array de los valores de columna.
-                arrayMinimos.pop(); 
-            }
+            arrayMinimos = [];      //inicializando array valores columna.
             calculoPosicion();
-            if(primerScroll){                       //Se establece la animación del primer scroll.  
-                $(window).scrollTop(0);
-                $('.caja-cabecera').addClass('recogida');
-                $('.tags').addClass('tag-desplegado');
-                setTimeout(function () {  primerScroll = false;}, 900);        
+            if(primerScroll){       //se establece animación primer scroll
+                window.scrollTo(0, 0);
+                document.querySelector('.caja-cabecera').classList.add('recogida');
+                document.querySelector('.tags').classList.add('tag-desplegado');
+                setTimeout(function () {  primerScroll = false;}, 900);
             }
         }
-    });
-
-
-     /*  $(window).on('touchstart', function () { 
-        dispositivoMovil = true;
-        $(window).off('touchstart')
-     }) */
-
-
-
-    /*imagen.on('mouseover', function() {                             //Cortina negra cd pasa el ratón.
-      //  if(modoMosaico & !dispositivoMovil){
-            $(this).find('.cortina').stop(true, false).slideDown();  //Función ejecutada cuando el ratón entra, en modo mosaico y si el dispositivo no es movil.
-        //}
-        /*if(dispositivoMovil){
-            imagen.off('mouseover');
+     });        
+           
+    botonMenu.addEventListener('change',function () { 
+        scrollPosition = window.scrollY;
+        if (aboutDesplegado) {
+            cortinaRoja.classList.replace('desplegado','replegado');
+           textoAbout.classList.replace('desplegado-texto', 'replegado-texto');
+            for (let i = 0; i < iconosRedes.length; i++) {
+                iconosRedes[i].classList.remove('mostrar');
+                
+            }
         }
-    }); 
-
-    imagen.on('mouseleave', function() {
-        $(this).find('.cortina').stop(true, false).slideUp(); //función ejecutada cuando el ratón sale.
-        if(dispositivoMovil){
-            imagen.off('mouseleave');
-            console.log('mouseleave desactivado');
-        }
-    });*/
-
-
-    $('#menu-icon').on('change',function(){             //Evento que calcula la posición de scroll cd se presiona el menú
-        scrollPosition = $(window).scrollTop();
-        if(aboutDesplegado){
-            $('.cortina-roja').removeClass('desplegado');
-            $('.texto-about').removeClass('desplegado-texto');
-            $('.cortina-roja').addClass('replegado');
-            $('.texto-about').addClass('replegado-texto');
-            $('.iconos-redes').removeClass('mostrar');
-            aboutDesplegado = false;
-        }
-    
-    });
-
-    imagen.click(function(){
-        $('.cortina').toggleClass('ocultar');
-        if(modoMosaico){
-            scrollPosition = $(window).scrollTop();
-            imagen.not(this).addClass('ocultar');
-            aleatorio = Math.ceil(Math.random()*4);
-            $(this).addClass('posicion-'+ aleatorio);
-            tamañoMaximoFotos(this);
-
-            modoMosaico = !modoMosaico;                   //Alternar valor booleano
-        }else{
-            imagen.not(this).removeClass('ocultar');
-            $(this).removeClass('posicion-'+ aleatorio);
-            $(this).find('img').css({
-                maxWidth: '',
-                maxHeight: '',
-                width: '100%'
-            });
-            modoMosaico = !modoMosaico;    
-        }
-        console.log('mostrar Mosaico: ' + modoMosaico + 'numero aleatorio: ' + aleatorio);
-
-    });
-
-    about.on('click', function () { 
-        $('.cortina-roja').removeClass('replegado');
-        $('.texto-about').removeClass('replegado-texto');
-        $('.cortina-roja').addClass('desplegado');
-        $('.texto-about').addClass('desplegado-texto');
-        $('.iconos-redes').addClass('mostrar');
-        aboutDesplegado = true;
      })
+
+  
+     for (let i = 0; i < imagen.length; i++) {
+         imagen[i].addEventListener('click', clickImagen);  
+     }
+
+    
+
+    about.addEventListener('click', function(){
+        cortinaRoja.classList.remove('replegado');
+        cortinaRoja.classList.add('desplegado');
+        textoAbout.classList.replace('replegado-texto', 'desplegado-texto');
+        for (let i = 0; i < iconosRedes.length; i++) {
+            iconosRedes[i].classList.add('mostrar');  
+        }
+        aboutDesplegado = true;
+    })
+
+/*------------------FUNCIÓN DE CLICK EN LAS IMÁGENES--------------------*/
+
+function clickImagen() {
+    this.querySelector('.cortina').classList.toggle('ocultar');
+    if(modoMosaico){
+       scrollPosition = window.scrollY;
+       for (let j = 0; j < imagen.length; j++) {
+           if(imagen[j] != this){
+               imagen[j].classList.add('ocultar');
+           }
+       }
+       aleatorio = Math.ceil(Math.random()*4);
+       this.classList.add('posicion-'+aleatorio);
+       tamañoMaximoFotos(this);
+       console.log('modo mosaico es ' + modoMosaico);
+       modoMosaico = !modoMosaico;                   //Alternar valor booleano
+       console.log('modo mosaico cambia a ' + modoMosaico);
+    }else{
+       for (let j = 0; j < imagen.length; j++) {
+           if(imagen[j] != this){
+               imagen[j].classList.remove('ocultar');
+               console.log('clase oculta');
+           }
+       }
+       this.classList.remove('posicion-'+ aleatorio);
+       let estiloImagen = this.querySelector('img');
+       estiloImagen.style.maxWidth = '';
+       estiloImagen.style.maxHeight = '';
+       estiloImagen.style.width = '100%';
+       modoMosaico = !modoMosaico;
+    }
+}
 
 /*----------------------FUNCIÓN DE CÁLCULO DE POSICIÓN-----------------------------*/
 
   function calculoPosicion() {
-      for (let n = 0; n < numeroColumnas; n++) {        //Inicializando el array de los valores de columna.
-          arrayMinimos.push(0); 
-      }
+    for (let n = 0; n < numeroColumnas; n++) {        //Inicializando el array de los valores de columna.
+        arrayMinimos.push(0); 
+    };
 
-        imagen.each(function () { 
-            var minimo = Math.min.apply(null, arrayMinimos);  //Se determina la altura de la columna más corta (coordenada Y)
-            var index = $.inArray(minimo, arrayMinimos);        //Se determina la columna que es
-            var posicionEjeX = index*anchoColumnas*anchoContenedor/100;     //Cálculo de la coordenada X
-            $(this).css({
-                'left': posicionEjeX+'px',          //Fijo coordenada X
-                'top': minimo+ 'px'                  //Fijo coordenada Y
-            });
-            
-            arrayMinimos[index] = minimo + $(this).outerHeight();       //Establezco la nueva altura de la columna
-
-         });
-
-         var maximo = Math.max.apply(null, arrayMinimos);
-         contenedor.css('height', maximo);
-      
+    for (let i = 0; i < imagen.length; i++) {
+        var minimo = Math.min.apply(null, arrayMinimos);    //Se determina la altura de la columna más corta (coordenada Y)
+        var index = arrayMinimos.indexOf(minimo);  //determina qué columna es
+        var posicionEjeX = index*anchoColumnas*anchoContenedor/100;    //Cálculo de la coordenada X
+        imagen[i].style.left = posicionEjeX + 'px';  //fija coordenada X
+        imagen[i].style.top = minimo + 'px';         //fija coordenada Y
+        arrayMinimos[index] = minimo + imagen[i].offsetHeight;  //establece nueva altura de la columna
+    }
+    var maximo = Math.max.apply(null, arrayMinimos);
+    contenedor.style.height = maximo + 'px';           //fijo altura del contenedor.
 
   }
 
@@ -165,7 +149,7 @@ $(document).ready(function () {
 
     function calcularColumnas(){
 
-        anchoContenedor = contenedor.width();
+        anchoContenedor = contenedor.offsetWidth;
         if(anchoContenedor<501){
         numeroColumnas = 1;
         }else if(anchoContenedor<1001){
@@ -176,7 +160,10 @@ $(document).ready(function () {
         numeroColumnas = 4;
         }
         anchoColumnas = 100/numeroColumnas;
-        imagen.css('width', anchoColumnas + '%');
+        imagen.forEach(function (value, index) { imagen[index].style.width = anchoColumnas + '%' });
+
+        console.log('el número de columnas es:' + numeroColumnas);
+        console.log('el ancho del contenedor es' + anchoContenedor);
     }
     
 
@@ -184,12 +171,10 @@ $(document).ready(function () {
 //--------------FUNCIÓN CÁLCULO ALTURA MÁXIMA FOTOS------------------
 
     function tamañoMaximoFotos(selector) {
-        let alturaMaxima = $('header nav').height();
-        $(selector).find('img').css({
-            maxWidth: anchoContenedor + 'px',
-            maxHeight: alturaMaxima + 'px',
-            width: 'auto'
-        });
+        let alturaMaxima = menu.offsetHeight;
+        selector.querySelector('img').style.maxWidth = anchoContenedor + 'px';
+        selector.querySelector('img').style.maxHeight = alturaMaxima + 'px';
+        selector.querySelector('img').style.width = 'auto';
     }
 
-});
+//});
